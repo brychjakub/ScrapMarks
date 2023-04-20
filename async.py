01ccfb352181ed2b0b38384985cdf4a130da0e8b
brychjakub <brychjakub@gmail.com>
2023-04-20 16:14:46 +0200

    handling wrong passwords

--- async.py
+++ async.py
@@ -56,14 +56,20 @@ async def choose_course(page):
         else:
             continue
 
-async def loginMicrosoft(page):
+
+async def login(email, password):
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch()
+        context = await browser.new_context()
+        page = await context.new_page()
+        await page.goto("https://uuidentity.plus4u.net/uu-identitymanagement-maing01/a9b105aff2744771be4daa8361954677/login?acrValues=low%20standard%20high%20veryHigh&clientId=07391806753a42cd8110140eb30683b9&uiLocales=cs&state=6jkPmRIkKca7teCO.g7RbWxfrgnrd7zkdlcEWqhNHlynG2db-zqZ2rcZPUQxsjoQxJHH4ktvOlg4-9EyfAmbUSla11voHKy3yBeaK-MoQ9cINLepbHmrdszPP-2kdbUQj8Hm_HKIM7KEhxZY25-jjuxh_zqALt2hkqD0EmYCDG-Oz4NP7dK-qDmrCyPnS3p5RSxImewr-KA4sYMpyMKo6L2wMF-ZUT-pEzMdDUOUs8hcOgzO5VCaEJxozpIFN2FIj_e5WsS_dLo2cmhvmXn1-DmjO4nSIr2q_DQYd2FGYy8uofvOh0llFjtZTg-BXswbhvIfTxrtKVnA1PKwVwfQHcHahisLC7zYgLm_DW5VJ3AE1HVzQ6YfiZmaMk9Ma9TPNA_7njXGrSOFp28zHBCZejycA3VTFzOUWUxAot3RtM0MJGYyshve63Fwg42oiSafcdkQOqEl3cdZMfHdaA8JHk3KZW2v1s5EfvbAzCR9t_XbfL7eOpLEnhrOuz3N33GodOhOagiFBKh46afREKQkG-JQ4VIGfjL90YAQN7iKI0Ai5ER6F8NYX1TieaAzS5Z55ANWzmaD8tSorB7wHYL8PaT2gCwfC2rdinliiMg1QHAfhVIz7SiPEvDN0")
+       
+        await page.wait_for_load_state("networkidle")
         button = await page.query_selector('button:has-text("Microsoft")')
-        await page.wait_for_load_state("networkidle")
-
         await button.click()
+                
 
         await page.wait_for_load_state("networkidle")
-
         email_selector = "input[type='email']"
         await page.fill(email_selector, f"{email}")
 
@@ -72,38 +78,26 @@ async def loginMicrosoft(page):
         sign_in_button_selector = "input[type='submit']"
         await page.click(sign_in_button_selector)
 
-        
-        staySignnedButton = "input[id='idBtn_Back']"
-
-        if staySignnedButton == False:
-            passwordAgain = getpass_asterisk("wrong password, try again: ")
-            password_selector = "input[type='password']"
-            await page.fill(password_selector, f"{passwordAgain}")
-        
-        else:
-            await page.wait_for_load_state("networkidle")
-        
+        await page.wait_for_load_state("networkidle")
+        password_selector = "input[type='password']"
+        await page.fill(password_selector, f"{password}")
         await page.click(sign_in_button_selector)
-        await page.click(staySignnedButton)
-
-        
-        
         await page.wait_for_load_state("networkidle")
 
+        while True:
+            try:
+                await page.wait_for_load_state("networkidle")
+                print("checking password")
+                dontKeepSignIn = "input[id='idBtn_Back']"
+                await page.click(dontKeepSignIn)
+                break
 
-async def main():
-    start_time = time.time()
-    print("start")
-
-    async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch()
-        context = await browser.new_context()
-        page = await context.new_page()
-        await page.goto("https://cmczs-login.edookit.net/")
-        button_selector = "#plus4ULoginButton"
-        await page.click(button_selector)
-        await page.wait_for_load_state("networkidle")
-        await loginMicrosoft(page)
+            except:
+                passwordAgain = getpass_asterisk("wrong password, please try again: ")
+                await page.fill(password_selector, f"{passwordAgain}")
+                await page.click(sign_in_button_selector)
+            
+            #await page.screenshot(path="example.png");
         evaluation_selector = "#lid221"
         await page.click(evaluation_selector)
 
@@ -112,6 +106,11 @@ async def main():
         await context.close()
         await browser.close()
 
+
+async def main():
+    start_time = time.time()
+    print("start")
+    await login(email, password)
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Elapsed time: {elapsed_time} seconds")

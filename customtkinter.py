19071e368e9900788bbf3679c7153e842e5128b2
brychjakub <brychjakub@gmail.com>
2023-04-25 07:15:30 +0200

    learning customtkinter

---
+++ customtkinter.py
@@ -0,0 +1,30 @@
+import customtkinter
+#import asynchro
+
+customtkinter.set_appearance_mode("Systerm")
+customtkinter.set_default_color_theme("green")
+
+
+root = customtkinter.CTk()
+root.geometry("500x350")
+
+
+def login():
+    print(entry1, entry2)
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System")
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+root.mainloop()
